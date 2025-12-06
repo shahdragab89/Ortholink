e523353f6381438f27b87a0392f118f2a71e083e
shahd <shahdragab89@gmail.com>
2025-12-06 22:11:39 +0200

    Update structure

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const BACKGROUND_IMAGE_URL = '/Background.png';
+
+export const COLORS = {
+    primary: '#5f9ea0',
+    primaryDark: '#4a7c7e',
+    secondary: '#0a586c',
+    doctorPrimary: '#02505F',
+    blue: '#4361ee',
+    // Add all your colors here
+};
